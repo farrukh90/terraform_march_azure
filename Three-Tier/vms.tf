a4c2dbbbd0940d287be26436eb868a240a390149
farrukh90 <farrukhsadykov@gmail.com>
2021-06-08 01:46:46 +0000

    removed interpolation

--- Three-Tier/vms.tf
+++ Three-Tier/vms.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_machine" "vm1" {
   name                  = var.vm1_computername
   location              = var.location
   resource_group_name   = var.resource_group_name
-  network_interface_ids = ["${azurerm_network_interface.nic1.id}"]
+  network_interface_ids = [azurerm_network_interface.nic1.id]
   vm_size               = var.vm_size
   storage_image_reference {
     publisher = var.publisher
@@ -31,7 +31,7 @@ resource "azurerm_virtual_machine" "vm2" {
   name                  = var.vm2_computername
   location              = var.location
   resource_group_name   = var.resource_group_name
-  network_interface_ids = ["${azurerm_network_interface.nic2.id}"]
+  network_interface_ids = [azurerm_network_interface.nic2.id]
   vm_size               = var.vm_size
   storage_image_reference {
     publisher = var.publisher
@@ -64,7 +64,7 @@ resource "azurerm_virtual_machine" "vm3" {
   name                  = var.vm3_computername
   location              = var.location
   resource_group_name   = var.resource_group_name
-  network_interface_ids = ["${azurerm_network_interface.nic3.id}"]
+  network_interface_ids = [azurerm_network_interface.nic3.id]
   vm_size               = var.vm_size
   storage_image_reference {
     publisher = var.publisher
