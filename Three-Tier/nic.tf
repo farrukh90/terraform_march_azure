c296312095bbf5c08f589038aa4fc14104e5c9ef
farrukh90 <farrukhsadykov@gmail.com>
2021-06-08 01:40:22 +0000

    added vm

--- Three-Tier/nic.tf
+++ Three-Tier/nic.tf
@@ -1,65 +1,65 @@
 resource "azurerm_network_interface" "nic1" {
-  name = "nic1"
-  location = var.location
+  name                = "nic1"
+  location            = var.location
   resource_group_name = var.resource_group_name
   ip_configuration {
-    name = "testconfiguration1"
-    subnet_id = azurerm_subnet.private1.id
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.private1.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.IP.id
+    public_ip_address_id          = azurerm_public_ip.IP.id
   }
-	tags = var.tags
+  tags = var.tags
 }
 
 
 resource "azurerm_network_interface" "nic2" {
-  name = "nic2"
-  location = var.location
+  name                = "nic2"
+  location            = var.location
   resource_group_name = var.resource_group_name
   ip_configuration {
-    name = "testconfiguration2"
-    subnet_id = azurerm_subnet.private2.id
+    name                          = "testconfiguration2"
+    subnet_id                     = azurerm_subnet.private2.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.IP2.id
+    public_ip_address_id          = azurerm_public_ip.IP2.id
   }
-	tags = var.tags
+  tags = var.tags
 }
 
 
 resource "azurerm_network_interface" "nic3" {
-  name = "nic3"
-  location = var.location
+  name                = "nic3"
+  location            = var.location
   resource_group_name = var.resource_group_name
   ip_configuration {
-    name = "testconfiguration3"
-    subnet_id = azurerm_subnet.private3.id
+    name                          = "testconfiguration3"
+    subnet_id                     = azurerm_subnet.private3.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.IP3.id
+    public_ip_address_id          = azurerm_public_ip.IP3.id
   }
-	tags = var.tags
+  tags = var.tags
 }
 
 
 resource "azurerm_public_ip" "IP" {
-  name = "public_ip"
-  location = var.location
+  name                = "public_ip"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method = "Dynamic"
-	tags = var.tags
+  allocation_method   = "Dynamic"
+  tags                = var.tags
 }
 
 resource "azurerm_public_ip" "IP2" {
-  name = "public_ip2"
-  location = var.location
+  name                = "public_ip2"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method = "Dynamic"
-	tags = var.tags
+  allocation_method   = "Dynamic"
+  tags                = var.tags
 }
 
 resource "azurerm_public_ip" "IP3" {
-  name = "public_ip3"
-  location = var.location
+  name                = "public_ip3"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method = "Dynamic"
-	tags = var.tags
+  allocation_method   = "Dynamic"
+  tags                = var.tags
 }
---
+++ Three-Tier/vms.tf
@@ -0,0 +1,94 @@
+resource "azurerm_virtual_machine" "vm1" {
+  name                  = var.vm1_computername
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = ["${azurerm_network_interface.nic1.id}"]
+  vm_size               = var.vm_size
+  storage_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.os_version
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = var.vm1_computername
+    admin_username = var.admin_username
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = var.tags
+}
+
+resource "azurerm_virtual_machine" "vm2" {
+  name                  = var.vm2_computername
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = ["${azurerm_network_interface.nic2.id}"]
+  vm_size               = var.vm_size
+  storage_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.os_version
+  }
+  storage_os_disk {
+    name              = "myosdisk2"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = var.vm2_computername
+    admin_username = var.admin_username
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "/home/centos/.ssh/authorized_keys"
+      key_data = file("~/.ssh/id_rsa.pub")
+
+    }
+  }
+  tags = var.tags
+}
+
+resource "azurerm_virtual_machine" "vm3" {
+  name                  = var.vm3_computername
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = ["${azurerm_network_interface.nic3.id}"]
+  vm_size               = var.vm_size
+  storage_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.os_version
+  }
+  storage_os_disk {
+    name              = "myosdisk3"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = var.vm3_computername
+    admin_username = var.admin_username
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "/home/centos/.ssh/authorized_keys"
+      key_data = file("~/.ssh/id_rsa.pub")
+
+    }
+  }
+  tags = var.tags
+}
